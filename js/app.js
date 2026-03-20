8914a25751d5a8355c41b9fbfed31dc5b2eda0a6
AlexisSerano <AlexisSerano.Pro@outlook.fr>
2026-03-20 11:51:09 +0100

    corrections app

--- js/app.js
+++ js/app.js
@@ -2,15 +2,28 @@
  * fichier qui démarre tout au chargement de la page. Il initialise le contrôleur.
  */
 
-import Crypto from './models/CryptoModel.js'; 
 import RechercheView from './views/RechercheView.js';
 import RechercheController from './controllers/RechercheController.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // On instancie la vue
-    const view = new RechercheView();
+import ResultatView from './views/ResultatView.js';
+import ResultatController from './controllers/ResultatController.js';
 
-    const app = new RechercheController(view); 
-    
-    app.init();
+document.addEventListener('DOMContentLoaded', () => {
+    const barreRecherche = document.getElementById("cryptoInput");
+    const logoResultat = document.querySelector(".main-logo");
+
+    if (barreRecherche) {
+        // ---> ON EST SUR LA PAGE ACCUEIL (index.html)
+        console.log("Démarrage du mode Recherche...");
+        const view = new RechercheView();
+        const app = new RechercheController(view); 
+        app.init();
+    } 
+    else if (logoResultat) {
+        // ---> ON EST SUR LA PAGE RÉSULTAT (resultat.html)
+        console.log("Démarrage du mode Résultat...");
+        const view = new ResultatView();
+        const app = new ResultatController(view);
+        app.init();
+    }
 });
