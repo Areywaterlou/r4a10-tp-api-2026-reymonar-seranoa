062b1ee81c7104d5fa5ded02a8f3602ad95593be
reymonar <areymond38160@gmail.com>
2026-03-20 09:14:15 +0100

    ajout des éléments dans l CryptoView via les id

---
+++ js/views/CryptoView.js
@@ -0,0 +1,13 @@
+/**
+ * Objet constant représentant la vue.
+ */
+export const view = {
+  
+  barreRecherche: document.getElementById("cryptoInput"),
+
+  btnRechercher: document.getElementById('searchButton'),
+
+  btnFavoris: document.getElementById("favoriteButton"),
+
+  favoriteContener: document.getElementById("favoritesList"),
+};
